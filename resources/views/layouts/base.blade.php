8104724b7271bc012a11cd993dca13df08483cc8
nakahara <nakahara@coremind.jp>
2020-08-04 12:02:11 +0900

    change: js/css loading with @stack/push

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,11 +1,23 @@
+@push('styles')
+  <link rel="stylesheet" href="{{ url('css/app.css') }} " />
+@endpush
+
+@push('scripts')
+  <script src="{{ url('/js/app.js') }} "></script>
+@endpush
+
 <! DOCTYPE html>
 <html>
-  <head>
-    <meta charset =" UTF-8" />
+  <head lang="ja">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>@yield('title', 'COREMIND')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    @stack('styles')
+    @stack('scripts')
   </head>
-  <body>
+  <body id="app">
     <x-header />
 
     @section('main')
