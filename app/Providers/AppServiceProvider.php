ef5f61f74d33ec8f5ea1c60efd8a240a07e1aa62
nakahara <nakahara@coremind.jp>
2020-09-14 00:07:26 +0900

    fix: "Specified key was too long;" when migrate.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+
         $this->registerCustomValidation();
         
         $this->registerComponents();
